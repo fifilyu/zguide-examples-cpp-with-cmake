b2f54e267904a985b4eda0772adfa4ef68594dc8
Fifi Lyu <fifilyu@gmail.com>
2015-09-22 15:19:49 +0800

    初始化项目

---
+++ msgqueue.cpp
@@ -0,0 +1,29 @@
+//
+//  Simple message queuing broker in C++
+//  Same as request-reply broker but using QUEUE device
+//
+// Olivier Chamoux <olivier.chamoux@fr.thalesgroup.com>
+
+
+#include "zhelpers.hpp"
+
+int main (int argc, char *argv[])
+{
+    zmq::context_t context(1);
+
+    //  Socket facing clients
+    zmq::socket_t frontend (context, ZMQ_ROUTER);
+    frontend.bind("tcp://*:5559");
+
+    //  Socket facing services
+    zmq::socket_t backend (context, ZMQ_DEALER);
+    zmq_bind (static_cast<void *>(backend), "tcp://*:5560");
+
+    //  Start built-in device
+    zmq_device (
+            ZMQ_QUEUE,
+            static_cast<void *>(frontend),
+            static_cast<void *>(backend)
+            );
+    return 0;
+}
